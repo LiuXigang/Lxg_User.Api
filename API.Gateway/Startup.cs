dfd4bb51d9b2665bbf7d5dd27dc6a4bc88d2922d
lxg <1436215718@qq.com>
2019-08-22 22:42:23 +0800

    Geteway(test)

---
+++ API.Gateway/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+namespace API.Gateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var authenticationProviderKey = "finbook";
+
+            services.AddAuthentication()
+            .AddIdentityServerAuthentication(authenticationProviderKey, options =>
+            {
+                options.Authority = "http://localhost:19936";
+                options.ApiName = "gateway_api";
+                options.SupportedTokens = SupportedTokens.Both;
+                options.ApiSecret = "secret";
+                options.RequireHttpsMetadata = false;
+            }
+             );
+            services.AddOcelot();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOcelot();
+        }
+    }
+}
